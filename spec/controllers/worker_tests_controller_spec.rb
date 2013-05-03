a5ff40559ed9c82a92262232f7a8753dfd019240
Michael Johnston <lastobelus@mac.com>
2013-05-03 14:09:53 -0700

    get hello streamworker spec passing

--- spec/controllers/worker_tests_controller_spec.rb
+++ spec/controllers/worker_tests_controller_spec.rb
@@ -6,7 +6,8 @@ describe WorkerTests do
   describe '#inspect' do
     it 'it renders a worker' do
       get :inspect
-      response.should contain("Listing widgets")
+      expect(response).to render_template("inspect")
+      expect(response.body).to include("#&lt;Streamworker::Workers::Worker")
     end
 
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,9 @@ SimpleCov.start.inspect
 require 'capybara/rspec'
 require 'be_valid_asset'
 
-Combustion.initialize! :active_record, :action_controller,
-                       :action_view, :sprockets
+Combustion.initialize! :active_record, :action_controller, :action_view, :sprockets do
+  require 'slim'
+end
 
 require 'rspec/rails'
 require 'capybara/rails'
